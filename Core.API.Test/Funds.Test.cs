6db2edffe3b5e82c3d84c0c4c52647c8c158dd2d
mdbhowmik <mdbhowmik@gmail.com>
2025-09-18 22:58:51 +0100

    Adding a test project. There is a error as the testproject is from 3.5 and above

---
+++ Core.API.Test/Funds.Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Core.API.Test
+{
+    [TestClass]
+    public class FundsTests
+    {
+        [TestMethod]
+        public void GetAllAsync_ReturnsFundDetails ()
+        {
+            // Arrange
+            var mockService = new MockExternalService();
+            var funds = new Funds(mockService);
+            // Act
+            var result = funds.GetFundDetails(null);
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, ((System.Collections.ICollection)result).Count);
+        }
+    }
+}
